7dd053f6e6a064ba5d821e74c3584571677deb84
GabrielSantillo <81308836+gabrielSantillo@users.noreply.github.com>
2022-12-08 10:15:29 -0700

    Making changes

--- dbcreds.py
+++
@@ -1,9 +0,0 @@
-# Here is the credential to connect to our database
-# I don't know how will we work here in the sense of what database name and password will we use
-# This will only work with we set up a name a password on line 7 and 8
-host='localhost'
-user='root'
-port=3306
-database=''
-password=''
-production_mode=False
